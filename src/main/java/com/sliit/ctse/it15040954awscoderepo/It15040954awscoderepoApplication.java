b2b641bac22298861dda1a96e39ed3b25a369edb
app_code_commit_user-at-134349149580 <dinusha.gammanpila@my.sliit.lk>
2018-05-24 22:40:03 +0530

    Initial Commit

---
+++ src/main/java/com/sliit/ctse/it15040954awscoderepo/It15040954awscoderepoApplication.java
@@ -0,0 +1,12 @@
+package com.sliit.ctse.it15040954awscoderepo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class It15040954awscoderepoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(It15040954awscoderepoApplication.class, args);
+	}
+}
---
+++ src/test/java/com/sliit/ctse/it15040954awscoderepo/It15040954awscoderepoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.sliit.ctse.it15040954awscoderepo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class It15040954awscoderepoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
